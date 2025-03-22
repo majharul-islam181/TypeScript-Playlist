6e2635339ffb281b68b0891f0176e0e7796133a5
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2025-03-23 00:01:10 +0600

    output

---
+++ output/script.js
@@ -0,0 +1,13 @@
+"use strict";
+let playerName = "Mashrafi";
+console.log(playerName);
+playerName = "12";
+console.log(playerName);
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(2, 3));
+//array
+let fruits = ['apple', 'banana', 'orange'];
+fruits.push('dd');
+console.log(fruits);
