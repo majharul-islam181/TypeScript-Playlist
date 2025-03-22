6802ba6981187c8de02bc7e0f4271d1ed7199f9d
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2025-03-23 00:00:01 +0600

    first commit

---
+++ src/script.ts
@@ -0,0 +1,17 @@
+let playerName = "Mashrafi";
+console.log(playerName)
+
+playerName = "12"
+console.log(playerName)
+
+function multiply(a: number , b: number){ //Explecit Type
+    return a * b;
+}
+
+console.log(multiply(2,3))
+
+
+//array
+let fruits = ['apple', 'banana', 'orange']
+fruits.push('dd')
+console.log(fruits)
