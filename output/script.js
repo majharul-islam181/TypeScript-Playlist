68153c9e42521505edd7e5929b86f9db41d9231d
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2025-03-23 00:09:02 +0600

    type selection

--- output/script.js
+++ output/script.js
@@ -8,6 +8,17 @@ function multiply(a, b) {
 }
 console.log(multiply(2, 3));
 //array
-let fruits = ['apple', 'banana', 'orange'];
+let fruits = ['apple', 'banana', 'orange']; // [[Prototype]] : Array
 fruits.push('dd');
 console.log(fruits);
+let mixed = ["banana", 23, false]; // [[Prototype]] : Array
+mixed.push(12.22);
+console.log(mixed);
+let person = {
+    name: "Majharul",
+    age: 22,
+    isCaptain: false,
+}; // [[Prototype]] : Object
+// person.age = "masjdfa",  // this got error
+person.age = 20;
+console.log(person);
--- src/script.ts
+++ src/script.ts
@@ -4,14 +4,29 @@ console.log(playerName)
 playerName = "12"
 console.log(playerName)
 
+
 function multiply(a: number , b: number){ //Explecit Type
     return a * b;
 }
-
 console.log(multiply(2,3))
 
 
 //array
-let fruits = ['apple', 'banana', 'orange']
+let fruits = ['apple', 'banana', 'orange']  // [[Prototype]] : Array
 fruits.push('dd')
 console.log(fruits)
+
+let mixed = ["banana",23,false]; // [[Prototype]] : Array
+mixed.push(12.22)
+console.log(mixed)
+
+let person = {
+    name : "Majharul",
+    age : 22,
+    isCaptain : false,
+}  // [[Prototype]] : Object
+
+// person.age = "masjdfa",  // this got error
+person.age = 20;
+console.log(person)
+
